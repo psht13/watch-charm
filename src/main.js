7a744497f6b86146c5661d9c1e13d611073f5c49
bndnt <67003594+bndnt@users.noreply.github.com>
2024-03-26 13:57:09 -0400

    debug+script

--- src/main.js
+++ src/main.js
@@ -57,3 +57,19 @@ for (let anchor of anchors) {
     });
   });
 }
+///load more
+
+let loadMoreBtn = document.querySelector('#js-load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.js-catalogue-block')];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = 'block';
+  }
+  currentItem += 3;
+
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = 'none';
+  }
+};
