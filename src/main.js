ff333e32d37f6ffea0737636bd94e1ecc29408e7
bndnt <67003594+bndnt@users.noreply.github.com>
2024-03-26 13:25:01 -0400

    debug

--- src/main.js
+++ src/main.js
@@ -1,46 +1,59 @@
+// mobile menu
+let openButton = document.getElementById('menu-open-btn');
+let removeButton = document.getElementById('menu-exit-btn');
+let element = document.getElementById('mobile-menu');
 
-  // mobile menu 
-  let openButton = document.getElementById('menu-open-btn');
-  let removeButton = document.getElementById('menu-exit-btn');
-  let element = document.getElementById('mobile-menu');
+openButton.addEventListener('click', function () {
+  element.classList.add('is-open');
+});
 
-  openButton.addEventListener('click', function () {
-    element.classList.add('is-open');
-  });
+removeButton.addEventListener('click', function () {
+  element.classList.remove('is-open');
+});
 
-  removeButton.addEventListener('click', function () {
-    element.classList.remove('is-open');
-  });
-
-
-  // sale
-  var swiper = new Swiper('.mySwiper', {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-    direction: 'vertical',
-    grabCursor: true,
-    mousewheel: true,
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        direction: 'horizontal',
-        spaceBetween: 5,
-        slidesPerView: 4,
-      },
-
-      // when window width is >= 768px
-      768: {
-        direction: 'vertical',
-        spaceBetween: 10,
-        slidesPerView: 4,
-      },
+// sale
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+  direction: 'vertical',
+  grabCursor: true,
+  mousewheel: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      direction: 'horizontal',
+      spaceBetween: 5,
+      slidesPerView: 4,
     },
-  });
-  var swiper2 = new Swiper('.mySwiper2', {
-    spaceBetween: 10,
-    thumbs: {
-      swiper: swiper,
+
+    // when window width is >= 768px
+    768: {
+      direction: 'vertical',
+      spaceBetween: 10,
+      slidesPerView: 4,
     },
+  },
+});
+var swiper2 = new Swiper('.mySwiper2', {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   });
+}
